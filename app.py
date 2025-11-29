52f7f18ded23b19e1f6f5e5b49c18be8a17a5748
deandrejef1919 <submittertruth19@gmail.com>
2025-11-29 08:38:32 -0600

    Update image_keys to include NANA_BULUKU_IMAGE_URL

--- app.py
+++ app.py
@@ -618,7 +618,7 @@ def page_gallery():
     )
 
     image_keys = [
-        ("Nana Buluku"),
+        ("NANA_BULUKU_IMAGE_URL", "Nana Buluku"),
         ("MAWU_LISA_IMAGE_URL", "Mawu-Lisa"),
         ("VODUN_ALTAR_IMAGE_URL", "Vodun Altar"),
         ("VODUN_DRUMMING_IMAGE_URL", "Vodun Drumming"),
